4b61c35fe17f240af93e49bae12073f7911849d5
Stephen Eley <sfeley@gmail.com>
2009-09-26 01:58:11 -0400

    Prepared MongoMapper connection params for production

--- config/initializers/mongomapper.rb
+++ config/initializers/mongomapper.rb
@@ -1,4 +1,10 @@
 require 'mongomapper'
 
 # Different databases by environment
+# We're using environment variables here to be friendly for Heroku hosting
+MongoMapper.connection = Mongo::Connection.new(ENV['FUN_MONGO_HOST']) if ENV['FUN_MONGO_HOST']
 MongoMapper.database = "fun_#{ENV['RAILS_ENV']}"
+
+if ENV['FUN_MONGO_USER'] && ENV['FUN_MONGO_PASS']
+  MongoMapper.database.authenticate(ENV['FUN_MONGO_USER'], ENV['FUN_MONGO_PASS'])
+end
